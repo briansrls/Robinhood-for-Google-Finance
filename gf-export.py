ac0158264f2211286427dff2dcd52fcad8005182
Brian Searls <briansrls@gmail.com>
2016-12-07 15:32:32 -0500

    replaced tabs with spaces

--- gf-export.py
+++ gf-export.py
@@ -22,18 +22,18 @@ robinhood = Robinhood();
 
 # login to Robinhood
 while not logged_in:
-	if username == "":
-		print("Robinhood username:")
-		try: input = raw_input
-		except NameError: pass
-		username = input()
-	if password == "":
-		password = getpass.getpass()
+    if username == "":
+        print("Robinhood username:")
+        try: input = raw_input
+        except NameError: pass
+        username = input()
+    if password == "":
+        password = getpass.getpass()
 
-	logged_in = robinhood.login(username=username, password=password)
-	if logged_in == False:
-		password = ""
-		print ("Invalid username or password.  Try again.\n")
+    logged_in = robinhood.login(username=username, password=password)
+    if logged_in == False:
+        password = ""
+        print ("Invalid username or password.  Try again.\n")
 
 fields = collections.defaultdict(dict)
 trade_count = 0
@@ -48,13 +48,13 @@ orders = robinhood.get_endpoint('orders')
 
 # store debug 
 if args.debug:
-	# save the CSV
-	try:
-	    with open("debug.txt", "w+") as outfile:
-	        outfile.write(str(orders))
-	        print("Debug infomation written to debug.txt")
-	except IOError:
-	    print('Oops.  Unable to write file to debug.txt')
+    # save the CSV
+    try:
+        with open("debug.txt", "w+") as outfile:
+            outfile.write(str(orders))
+            print("Debug infomation written to debug.txt")
+    except IOError:
+        print('Oops.  Unable to write file to debug.txt')
 
 # do/while for pagination
 
@@ -62,40 +62,40 @@ if args.debug:
 paginated = True
 page = 0
 while paginated:
-	for i, order in enumerate(orders['results']):
-		executions = order['executions']
-		
-		instrument = robinhood.get_custom_endpoint(order['instrument'])
-		fields[i+(page*100)]['symbol'] = instrument['symbol']
-		for key, value in enumerate(order):
-			if value != "executions":
-				fields[i+(page*100)][value] = order[value]
-		if order['state'] == "filled":
-			trade_count += 1
-			for key, value in enumerate(executions[0]):
-			 	fields[i+(page*100)][value] = executions[0][value]
-		elif order['state'] == "queued":
-			queued_count += 1
-	# paginate, if out of ORDERS paginate is OVER
-	if orders['next'] is not None:
-		page = page + 1
-		#get the next order, a page is essentially one order
-		orders = robinhood.get_custom_endpoint(str(orders['next']))
-	else:
-		paginated = False
+    for i, order in enumerate(orders['results']):
+        executions = order['executions']
+        
+        instrument = robinhood.get_custom_endpoint(order['instrument'])
+        fields[i+(page*100)]['symbol'] = instrument['symbol']
+        for key, value in enumerate(order):
+            if value != "executions":
+                fields[i+(page*100)][value] = order[value]
+        if order['state'] == "filled":
+            trade_count += 1
+            for key, value in enumerate(executions[0]):
+                 fields[i+(page*100)][value] = executions[0][value]
+        elif order['state'] == "queued":
+            queued_count += 1
+    # paginate, if out of ORDERS paginate is OVER
+    if orders['next'] is not None:
+        page = page + 1
+        #get the next order, a page is essentially one order
+        orders = robinhood.get_custom_endpoint(str(orders['next']))
+    else:
+        paginated = False
 
 #for i in fields:
-# 	print fields[i]
-# 	print "-------"	
+#     print fields[i]
+#     print "-------"    
 #Fields stores ALL relevant information
 
 # check we have trade data to export
 if trade_count > 0 or queded_count > 0:
-	print("%d queued trade%s and %d executed trade%s found in your account." % (queued_count, "s"[queued_count==1:], trade_count, "s"[trade_count==1:]))
-	# print str(queued_count) + " queded trade(s) and " + str(trade_count) + " executed trade(s) found in your account."
+    print("%d queued trade%s and %d executed trade%s found in your account." % (queued_count, "s"[queued_count==1:], trade_count, "s"[trade_count==1:]))
+    # print str(queued_count) + " queded trade(s) and " + str(trade_count) + " executed trade(s) found in your account."
 else:
-	print("No trade history found in your account.")
-	quit()
+    print("No trade history found in your account.")
+    quit()
 
 # CSV headers
 
@@ -107,32 +107,32 @@ keys = fields[0].keys()
 keys = sorted(keys)
 newkeys = []
 for key in keys:
-	if key in desired:
-		newkeys.append(key)
+    if key in desired:
+        newkeys.append(key)
 
 keys = list(newkeys)
 for i in range(0, len(newkeys)):
-	if newkeys[i] == "price": 
-		newkeys[i] = "Purchase price per share"
-	if newkeys[i] == "created_at": 
-		newkeys[i] = "Date purchased"
-	if newkeys[i] == "fees": 
-		newkeys[i] = "Commission"
-	if newkeys[i] == "quantity": 
-		newkeys[i] = "Shares"
-	if newkeys[i] == "symbol": 
-		newkeys[i] = "Symbol"
-	if newkeys[i] == "side": 
-		newkeys[i] = "Transaction type"
-	if newkeys[i] == "state":
-		newkeys[i] = "State"
-	if newkeys[i] == "cancel":
-		newkeys[i] = "Cancelled"
+    if newkeys[i] == "price": 
+        newkeys[i] = "Purchase price per share"
+    if newkeys[i] == "created_at": 
+        newkeys[i] = "Date purchased"
+    if newkeys[i] == "fees": 
+        newkeys[i] = "Commission"
+    if newkeys[i] == "quantity": 
+        newkeys[i] = "Shares"
+    if newkeys[i] == "symbol": 
+        newkeys[i] = "Symbol"
+    if newkeys[i] == "side": 
+        newkeys[i] = "Transaction type"
+    if newkeys[i] == "state":
+        newkeys[i] = "State"
+    if newkeys[i] == "cancel":
+        newkeys[i] = "Cancelled"
 
 csv = ""
 for key in newkeys:
-	if key != "State" and key != "Cancelled":
-		csv += key + ','
+    if key != "State" and key != "Cancelled":
+        csv += key + ','
 csv += "\n"
 
 # CSV rows
@@ -141,21 +141,21 @@ line = ""
 csvb = []
 
 for row in fields:
-	for key in keys:
-		if str(fields[row]["state"]) == "filled" and str(fields[row]["cancel"]) == "None":
-	  		try:
-				if key!="state" and key!="cancel":
-					line += str(fields[row][key]) + ","
-			except:
-				line += ","
-	if str(fields[row]["state"]) == "filled":
-		line += "\n"
-	csvb.append(line)
-	line = ""
+    for key in keys:
+        if str(fields[row]["state"]) == "filled" and str(fields[row]["cancel"]) == "None":
+            try:
+                if key!="state" and key!="cancel":
+                    line += str(fields[row][key]) + ","
+            except:
+                line += ","
+    if str(fields[row]["state"]) == "filled":
+        line += "\n"
+    csvb.append(line)
+    line = ""
 
 #google finance seems to prefer dates in ascending order, so we must reverse the given order
 for i in reversed(csvb):
-	csv+=str(i)
+    csv+=str(i)
 
 # choose a filename to save to
 print("Choose a filename or press enter to save to `robinhood.csv`:")
@@ -163,7 +163,7 @@ try: input = raw_input
 except NameError: pass
 filename = input().strip()
 if filename == '':
-	filename = "robinhood.csv"
+    filename = "robinhood.csv"
 
 # save the CSV
 try:
