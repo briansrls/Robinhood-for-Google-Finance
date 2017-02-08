119e336cba888d76e43c6710d16a1fb4631bdf49
Brian Searls <briansrls@gmail.com>
2017-02-08 13:50:04 -0500

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,4 @@
 ::This first line runs pip, which is a package manager available for python  It downloads and installs necessary python libraries, and in our case it will read the list of needed libraries from the file called "requirements.txt"
-
 ::In this case, the only library we need is called "requests", so this line is equivalent to "pip install requirements".  The librarie names needed are stored in this text file in case more are needed in the future, you will only need to run one command vs multiple.
 pip install requirements.txt
 
