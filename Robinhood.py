3466d0db9c6e367b72d74fd16b474b8195b3aa76
Brian Searls <briansrls@gmail.com>
2017-03-03 20:01:07 -0500

    Integrated MFA changes to Robinhood.py

--- Robinhood.py
+++ Robinhood.py
@@ -1,47 +1,52 @@
+
+import getpass
 import json
 import requests
 import urllib
 import sys
-
-try:
-    from urllib.request import urlretrieve  #py3
+try:     
+    from urllib.request import urlretrieve  #py3 
 except ImportError:
     from urllib import urlretrieve  # py2
 
 class Robinhood:
 
     endpoints = {
+            "login": "https://api.robinhood.com/api-token-auth/",
+            "investment_profile": "https://api.robinhood.com/user/investment_profile/",
             "accounts":"https://api.robinhood.com/accounts/",
             "ach_iav_auth":"https://api.robinhood.com/ach/iav/auth/",
             "ach_relationships":"https://api.robinhood.com/ach/relationships/",
             "ach_transfers":"https://api.robinhood.com/ach/transfers/",
             "applications":"https://api.robinhood.com/applications/",
             "dividends":"https://api.robinhood.com/dividends/",
-            "document_requests":"https://api.robinhood.com/upload/document_requests/",
             "edocuments":"https://api.robinhood.com/documents/",
-            "employment":"https://api.robinhood.com/user/employment",
             "instruments":"https://api.robinhood.com/instruments/",
-            "investment_profile": "https://api.robinhood.com/user/investment_profile/",
-            "login": "https://api.robinhood.com/api-token-auth/",
             "margin_upgrades":"https://api.robinhood.com/margin/upgrades/",
-            "markets":"https://api.robinhood.com/markets/", 
-            "notification_settings":"https://api.robinhood.com/settings/notifications/",
+            "markets":"https://api.robinhood.com/markets/",
             "notifications":"https://api.robinhood.com/notifications/",
             "orders":"https://api.robinhood.com/orders/",
             "password_reset":"https://api.robinhood.com/password_reset/request/",
             "portfolios":"https://api.robinhood.com/portfolios/",
             "positions":"https://api.robinhood.com/positions/",
             "quotes":"https://api.robinhood.com/quotes/",
+            "historicals":"https://api.robinhood.com/quotes/historicals/",
+            "document_requests":"https://api.robinhood.com/upload/document_requests/",
             "user":"https://api.robinhood.com/user/",
-            "watchlists":"https://api.robinhood.com/watchlists/"
+            "watchlists":"https://api.robinhood.com/watchlists/",
+            "news":"https://api.robinhood.com/midlands/news/"
     }
 
     session = None
+
     username = None
+
     password = None
+
     headers = None
+
     auth_token = None
-    positions = None
+
 
     ##############################
     #Logging in and initializing
@@ -50,9 +55,10 @@ class Robinhood:
     def __init__(self):
         self.session = requests.session()
         try:
-            self.session.proxies = urllib.getproxies() #py2
+	        self.session.proxies = urllib.getproxies() #py2
         except:
-            self.session.proxies = urllib.request.getproxies() #py3
+                self.session.proxies = urllib.request.getproxies() #py3
+
         self.headers = {
             "Accept": "*/*",
             "Accept-Encoding": "gzip, deflate",
@@ -64,18 +70,45 @@ class Robinhood:
         }
         self.session.headers = self.headers
 
+    def login_prompt(self):
+        """Prompts user for username and password and calls login()."""
+        username = raw_input("Username: ")
+        password = getpass.getpass()
+        return self.login(username=username, password=password)
+
     def login(self, username, password):
         self.username = username
         self.password = password
+        #data = urllib.urlencode({"password" : self.password, "username" : self.username})
         if sys.version_info[0] < 3:
-                data = urllib.urlencode({"password" : self.password, "username" : self.username})
+            data = urllib.urlencode({"password" : self.password, "username" : self.username})
         else:
-                data = urllib.parse.urlencode({"password" : self.password, "username" : self.username})
+            data = urllib.parse.urlencode({"password" : self.password, "username" : self.username})
         res = self.session.post(self.endpoints['login'], data=data)
         res = res.json()
-        try:
-            self.auth_token = res['token']
-        except KeyError:
+        if 'token' in res:
+             self.auth_token = res['token']
+             print('Login successfully')
+        elif 'mfa_type' in res:  # When MFA is turned on
+             if sys.version_info[0] < 3:
+                 mfa_code = raw_input("MFA code: ").strip()
+             else:
+                 mfa_code = input("MFA code: ").strip()
+	
+             if sys.version_info[0] < 3:
+                 data = urllib.urlencode({"password" : self.password, "username" : self.username, 'mfa_code': mfa_code})
+             else:
+                 data = urllib.parse.urlencode({"password" : self.password, "username" : self.username, 'mfa_code': mfa_code})
+             res_mfa = self.session.post(self.endpoints['login'], data=data)
+             res_mfa = res_mfa.json()
+             if 'token' in res_mfa:
+                 self.auth_token = res_mfa['token']
+                 print('Login successfully')
+             else:
+                 print('Login Failed')
+                 return False
+        else:
+            print('Login Failed')
             return False
         self.headers['Authorization'] = 'Token '+self.auth_token
         return True
@@ -96,10 +129,7 @@ class Robinhood:
         self.session.get(self.endpoints['investment_profile'])
 
     def instruments(self, stock=None):
-        if stock == None:
-            res = self.session.get(self.endpoints['instruments'])
-        else:
-            res = self.session.get(self.endpoints['instruments'], params={'query':stock.upper()})
+        res = self.session.get(self.endpoints['instruments'], params={'query':stock.upper()})
         res = res.json()
         return res['results']
 
@@ -110,7 +140,7 @@ class Robinhood:
         url = str(self.endpoints['quotes']) + str(stock) + "/"
         #Check for validity of symbol
         try:
-            res = json.loads((urllib.urlopen(url)).read());
+            res = json.loads((urllib.request.urlopen(url)).read().decode('utf-8'));
             if len(res) > 0:
                 return res;
             else:
@@ -121,6 +151,19 @@ class Robinhood:
     def get_quote(self, stock=None):
         data = self.quote_data(stock)
         return data["symbol"]
+
+    def get_historical_quotes(self,symbol,interval,span,bounds='regular'):
+        # Valid combination
+        # interval = 5minute | 10minute + span = day, week
+        # interval = day + span = year
+        # interval = week
+        # bounds can be 'regular' for regular hours or 'extended' for extended hours
+        res = self.session.get(self.endpoints['historicals'], params={'symbols':','.join(symbol).upper(), 'interval':interval, 'span':span, 'bounds':bounds})
+        return res.json()
+        
+    def get_news(self, symbol):
+        return self.session.get(self.endpoints['news']+symbol.upper()+"/").json()
+        
 
     def print_quote(self, stock=None):
         data = self.quote_data(stock)
@@ -145,9 +188,6 @@ class Robinhood:
     def last_trade_price(self, stock=None):
         return self.quote_data(stock)['last_trade_price'];
 
-    def last_trade_price(self, stock=None):
-        return self.quote_data(stock)['last_trade_price'];
-
     def previous_close(self, stock=None):
         return self.quote_data(stock)['previous_close'];
 
@@ -162,18 +202,92 @@ class Robinhood:
 
     def last_updated_at(self, stock=None):
         return self.quote_data(stock)['updated_at'];
+    
+    def get_account(self):
+        res = self.session.get(self.endpoints['accounts'])
+        res = res.json()
+        return res['results'][0]
+        
+    def get_url(self,url):
+        return self.session.get(url).json()
+
+    ##############################
+    # PORTFOLIOS DATA
+    ##############################
+
+    def portfolios(self):
+        """Returns the user's portfolio data."""
+        return self.session.get(self.endpoints['portfolios']).json()['results'][0]
+
+    def adjusted_equity_previous_close(self):
+        return float(self.portfolios()['adjusted_equity_previous_close'])
+
+    def equity(self):
+        return float(self.portfolios()['equity'])
+
+    def equity_previous_close(self):
+        return float(self.portfolios()['equity_previous_close'])
+
+    def excess_margin(self):
+        return float(self.portfolios()['excess_margin'])
+
+    def extended_hours_equity(self):
+        return float(self.portfolios()['extended_hours_equity'])
+
+    def extended_hours_market_value(self):
+        return float(self.portfolios()['extended_hours_market_value'])
+
+    def last_core_equity(self):
+        return float(self.portfolios()['last_core_equity'])
+
+    def last_core_market_value(self):
+        return float(self.portfolios()['last_core_market_value'])
+
+    def market_value(self):
+        return float(self.portfolios()['market_value'])
+        
+    def order_history(self):
+        return self.session.get(self.endpoints['orders']).json()
+        
+    def dividends(self):
+        return self.session.get(self.endpoints['dividends']).json()
+
+    ##############################
+    # POSITIONS DATA
+    ##############################
+
+    def positions(self):
+        """Returns the user's positions data."""
+        return self.session.get(self.endpoints['positions']).json()
+
+    def securities_owned(self):
+        """
+        Returns a list of symbols of securities of which there are more
+        than zero shares in user's portfolio.
+        """
+        positions = self.positions()
+        securities = []
+        for position in positions['results']:
+            quantity = float(position['quantity'])
+            if quantity > 0:
+                securities.append(self.session.get(position['instrument']).json()['symbol'])
+        return securities
 
     ##############################
     #PLACE ORDER
     ##############################
 
     def place_order(self, instrument, quantity=1, bid_price = None, transaction=None):
-        # cache the account ID that's needed for placing orders
-        if self.positions == None:
-            self.positions = self.get_endpoint("positions")['results']
         if bid_price == None:
-            bid_price = self.quote_data(instrument['symbol'])[0]['bid_price']
-        data = 'account=%s&instrument=%s&price=%f&quantity=%d&side=buy&symbol=%s&time_in_force=gfd&trigger=immediate&type=market' % (urllib.quote(self.positions[0]['account']), urllib.unquote(instrument['url']), float(bid_price), quantity, instrument['symbol']) 
+            bid_price = self.quote_data(instrument['symbol'])['bid_price']
+        data = 'account=%s&instrument=%s&price=%f&quantity=%d&side=%s&symbol=%s&time_in_force=gfd&trigger=immediate&type=market' % (
+            self.get_account()['url'],
+            urllib.unquote(instrument['url']),
+            float(bid_price),
+            quantity,
+            transaction,
+            instrument['symbol']
+        ) 
         res = self.session.post(self.endpoints['orders'], data=data)
         return res
 
